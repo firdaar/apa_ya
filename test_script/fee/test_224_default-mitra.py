5638dc1ed8fcbc281b3227e86c15f23bfeaaf5ea
Gusba <82691411+bagusghani001@users.noreply.github.com>
2022-02-25 11:44:23 +0700

    Update test_224_default-mitra.py

--- test_script/fee/test_224_default-mitra.py
+++ test_script/fee/test_224_default-mitra.py
@@ -62,24 +62,22 @@ def test_normal():
   req = requests.post(http_endpoint, json=json.loads(post_data))
   resp = req.json()
   logging.debug(resp)
-  print(resp)
-  assert resp.get('rows')!=None
-  rows = resp.get('rows')
-  for row in rows.count():
-    assert resp.get('row').get('id') !=None
-    assert resp.get('row').get('trx_type_source') !=None
-    assert resp.get('row').get('jenis_trx') !=None
-    assert resp.get('row').get('industri') !=None
-    assert resp.get('row').get('trx_type_target') !=None
-    assert resp.get('row').get('biller_target') !=None
-    assert resp.get('row').get('trx_type_source_desc') !=None
-    assert resp.get('row').get('channel_source') !=None
-    assert resp.get('row').get('nom_min') !=None
-    assert resp.get('row').get('nom_max') !=None
-    assert resp.get('row').get('fee_perorangan') !=None
-    assert resp.get('row').get('fee_super_agen') !=None
-    assert resp.get('row').get('biaya_loket') !=None
   assert resp.get('count')!=None
+  for row in range(resp.get('count')):
+    assert resp.get('rows')[row].get('id') !=None
+    assert resp.get('rows')[row].get('trx_type_source') !=None
+    assert resp.get('rows')[row].get('jenis_trx') !=None
+    assert resp.get('rows')[row].get('industri') !=None
+    assert resp.get('rows')[row].get('trx_type_target') !=None
+    assert resp.get('rows')[row].get('biller_target') !=None
+    assert resp.get('rows')[row].get('trx_type_source_desc') !=None
+    assert resp.get('rows')[row].get('channel_source') !=None
+    assert resp.get('rows')[row].get('nom_min') !=None
+    assert resp.get('rows')[row].get('nom_max') !=None
+    assert resp.get('rows')[row].get('fee_perorangan') !=None
+    assert resp.get('rows')[row].get('fee_super_agen') !=None
+    assert resp.get('rows')[row].get('biaya_loket') !=None
+
 
 
 ### TC Abnormal - Error SOA ###
@@ -167,20 +165,20 @@ def test_bypass_session():
   req = requests.post(http_endpoint, json=json.loads(post_data))
   resp = req.json()
   logging.debug(resp)
+  print(resp)
   assert resp.get('rows')!=None
-  rows = resp.get('rows')
-  for row in rows.count():
-    assert resp.get('row').get('id') !=None
-    assert resp.get('row').get('trx_type_source') !=None
-    assert resp.get('row').get('jenis_trx') !=None
-    assert resp.get('row').get('industri') !=None
-    assert resp.get('row').get('trx_type_target') !=None
-    assert resp.get('row').get('biller_target') !=None
-    assert resp.get('row').get('trx_type_source_desc') !=None
-    assert resp.get('row').get('channel_source') !=None
-    assert resp.get('row').get('nom_min') !=None
-    assert resp.get('row').get('nom_max') !=None
-    assert resp.get('row').get('fee_perorangan') !=None
-    assert resp.get('row').get('fee_super_agen') !=None
-    assert resp.get('row').get('biaya_loket') !=None
+  for row in range(resp.get('count')):
+    assert resp.get('rows')[row].get('id') !=None
+    assert resp.get('rows')[row].get('trx_type_source') !=None
+    assert resp.get('rows')[row].get('jenis_trx') !=None
+    assert resp.get('rows')[row].get('industri') !=None
+    assert resp.get('rows')[row].get('trx_type_target') !=None
+    assert resp.get('rows')[row].get('biller_target') !=None
+    assert resp.get('rows')[row].get('trx_type_source_desc') !=None
+    assert resp.get('rows')[row].get('channel_source') !=None
+    assert resp.get('rows')[row].get('nom_min') !=None
+    assert resp.get('rows')[row].get('nom_max') !=None
+    assert resp.get('rows')[row].get('fee_perorangan') !=None
+    assert resp.get('rows')[row].get('fee_super_agen') !=None
+    assert resp.get('rows')[row].get('biaya_loket') !=None
   assert resp.get('count')!=None
